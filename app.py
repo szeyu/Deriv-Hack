ac14919ab0b5b88feaf5c79a89830739c3e6268b
szeyu <szeyusim@gmail.com>
2024-11-09 15:05:16 +0800

    Comment out PDF to PNG conversion logic in app.py

--- app.py
+++ app.py
@@ -27,32 +27,32 @@ if "upscaled" not in st.session_state:
 
 # Main app logic
 def main():
-    logging.basicConfig(level=logging.INFO)
-    st.title('PDF to PNG Converter')
+    # logging.basicConfig(level=logging.INFO)
+    # st.title('PDF to PNG Converter')
 
-    uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+    # uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
     
-    if uploaded_file is not None:
-        # Write the file to a temporary location on disk
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
-            tmp_file.write(uploaded_file.getbuffer())
-            pdf_file_path = tmp_file.name
+    # if uploaded_file is not None:
+    #     # Write the file to a temporary location on disk
+    #     with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+    #         tmp_file.write(uploaded_file.getbuffer())
+    #         pdf_file_path = tmp_file.name
         
-        # Create a temporary output folder for PNG files
-        with tempfile.TemporaryDirectory() as output_folder:
-            # Call the pdf_to_png function
-            st.info("Converting PDF to PNG images...")
-            pdf_to_png(pdf_file_path, output_folder)
+    #     # Create a temporary output folder for PNG files
+    #     with tempfile.TemporaryDirectory() as output_folder:
+    #         # Call the pdf_to_png function
+    #         st.info("Converting PDF to PNG images...")
+    #         pdf_to_png(pdf_file_path, output_folder)
             
-            # Display the PNG images
-            st.success("Conversion completed! Displaying images:")
-            for file_name in sorted(os.listdir(output_folder)):
-                if file_name.endswith(".png"):
-                    image_path = os.path.join(output_folder, file_name)
-                    # st.image(image_path, caption=file_name)
-                    resized = nearestNeighboor(image_path, 1.5)
-                    st.image(resized, caption= file_name) 
-                    st.success("Upscaling completed!")
+    #         # Display the PNG images
+    #         st.success("Conversion completed! Displaying images:")
+    #         for file_name in sorted(os.listdir(output_folder)):
+    #             if file_name.endswith(".png"):
+    #                 image_path = os.path.join(output_folder, file_name)
+    #                 # st.image(image_path, caption=file_name)
+    #                 resized = nearestNeighboor(image_path, 1.5)
+    #                 st.image(resized, caption= file_name) 
+    #                 st.success("Upscaling completed!")
             
     if st.session_state.page == "passport":
         passport.show()
